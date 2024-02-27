25da59f6ebfc136d5b61b91d77997cf1fdc8fdfb
kvchi <jonathanmkpuma@gmail.com>
2024-02-27 21:55:20 +0800

    initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -16,18 +16,19 @@ route.get("/register", (req, res) => {
 
 // POST
 route.post('/register', async (req, res) => {
+
     try {
         const { username, password} = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const lowerUsername = username.toLowerCase()
 
         db.query(`SELECT id FROM ${table} WHERE username = ?`, [lowerUsername], (err, data) => {
-            if(err) return res.status(500).json({error: true, message: `Something went wrong, we could not process your registration.`})
+            if(err) return res.status(500).json({error: true, message: `Error trying to find the user.`})
             if(data.length) return res.status(403).json({error: true, message: `This username already exist, please try another`})
             else {
                 db.query(`INSERT INTO ${table} (username, password) VALUES (?,?)`, [lowerUsername, hashedPassword], (err, data) => {
                     // if(err) res.status(500).json({error: true, message: `Something went wrong, we could not process your registration. ${err}`}) //Include the ${err} to see the cause ONLY in development
-                    if(err) return res.status(500).json({error: true, message: `Something went wrong, we could not process your registration.`})
+                    if(err) return res.status(500).json({error: true, message: `Error after inserting into the table.`})
                     return res.status(201).json({error: false, message: `Welcome to NathanBlog ${username}`, data})
                 })
             }
@@ -35,6 +36,7 @@ route.post('/register', async (req, res) => {
 
     } catch (error) {
         console.error(error);
+        console.log(res.json())
         return res.status(500).json({error: true, message: `Something went wrong, we could not process your registration`});
     }
 });
